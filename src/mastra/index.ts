667e48448b2cb6f2db2088282043aca3a70ec0b1
PaulAjii <paulajii134@gmail.com>
2025-11-04 15:02:17 +0100

    feat: registered the a2aRoute in index file

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,6 +1,26 @@
 import { Mastra } from "@mastra/core";
 import { swiftDoc } from "./agent/agent";
+import { LibSQLStore } from "@mastra/libsql";
+import { PinoLogger } from "@mastra/loggers";
+import { a2aAgentRoute } from "./routes/a2a.route";
 
 export const mastra = new Mastra({
   agents: { swiftDoc },
+  storage: new LibSQLStore({ url: ":memory:" }),
+  logger: new PinoLogger({
+    name: "Mastra",
+    level: "debug",
+  }),
+  observability: {
+    default: {
+      enabled: true,
+    },
+  },
+  server: {
+    build: {
+      openAPIDocs: true,
+      swaggerUI: true,
+    },
+    apiRoutes: [a2aAgentRoute],
+  },
 });
