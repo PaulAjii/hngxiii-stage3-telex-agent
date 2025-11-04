2a3db0aa78c3eb01a18bcb3a3ee1e4ae20247a02
PaulAjii <paulajii134@gmail.com>
2025-11-04 14:32:27 +0100

    feat: created add to pincecone database tol

---
+++ src/mastra/tools/add-to cache.ts
@@ -0,0 +1,32 @@
+import { createTool } from "@mastra/core/tools";
+import { z } from "zod";
+import { createMultipleEmbeddings } from "../utils/embedding-util";
+import store from "../config/db";
+
+export const addToCacheTool = createTool({
+  id: "add-to-cache",
+  description: "Add output from search-web-for-context to vector database",
+  inputSchema: z.object({
+    output: z.string(),
+  }),
+  execute: async ({ context }) => {
+    try {
+      const { chunks, multipleEmbeddings } = await createMultipleEmbeddings(
+        context.output
+      );
+
+      console.log("Saving into the database....");
+      await store.upsert({
+        indexName: "hngindex",
+        vectors: multipleEmbeddings,
+        metadata: chunks.map((c) => ({
+          text: c.text,
+          id: c.id_,
+        })),
+      });
+      console.log("Saved the embeddigns into the database");
+    } catch (error) {
+      console.log(`There was an error adding to cache: ${error}`);
+    }
+  },
+});
