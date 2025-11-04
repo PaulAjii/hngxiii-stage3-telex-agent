763e3a7da75a6e0e8941dacf5b9f75be1c45aa74
PaulAjii <paulajii134@gmail.com>
2025-11-04 14:29:22 +0100

    feat: created get cached answer to retrieve response from db

---
+++ src/mastra/tools/get-cached-answer.ts
@@ -0,0 +1,37 @@
+import { createTool } from "@mastra/core/tools";
+import { z } from "zod";
+import { createSingleEmbedding } from "../utils/embedding-util";
+import store from "../config/db";
+
+export const getCachedAnswerTool = createTool({
+  id: "get-cached-answer",
+  description:
+    "Checks if a relevant answer already exists in the vector database. It takes the user's query, creates an embedding, and searches the database",
+  inputSchema: z.object({
+    query: z.string(),
+  }),
+  outputSchema: z.object({
+    output: z.string(),
+  }),
+  execute: async ({ context }) => {
+    const embeddings = await createSingleEmbedding(context.query);
+
+    const result = await store.query({
+      indexName: "hngindex",
+      queryVector: embeddings,
+      topK: 10,
+      includeVector: true,
+    });
+
+    const texts = result.map((res) => {
+      if (!res.metadata) {
+        return "";
+      }
+      return res.metadata.text;
+    });
+
+    return {
+      output: texts.join("\n"),
+    };
+  },
+});
